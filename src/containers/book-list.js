3e294d0127ee6ce285685ec823afffc6f3ab446c
zylstraj <colbertlove12@gmail.com>
2017-03-21 11:54:30 -0700

    Finished Redux Intro.
    Everything is worked. Basic look into Action Creators - Actions and
    Reducers.

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class BookList extends React.Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
-        <li key={book.title} className="list-group-item">{book.title}</li>
+        <li key={book.title}
+        onClick={() => this.props.selectBook(book)}
+        className="list-group-item">
+        {book.title}
+        </li>
       )
     })
   }
@@ -19,10 +25,17 @@ class BookList extends React.Component {
     )
   }
 }
+// Anything returned from this function will end up as props
+// on the BookList container
 function mapStateToProps(state) {
   return {
     books: state.books
   };
 }
+// Whenever selectBook is called - the result should be passed to all of our reducer_books
+//dispatch takes all the actions and spits it out to all the reducers
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ selectBook: selectBook }, dispatch);
+}
 
-export default connect(mapStateToProps)(BookList);
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
