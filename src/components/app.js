59b2689c36b3b24ca5bd5175b59b3c4aa4f5cf70
zylstraj <colbertlove12@gmail.com>
2017-03-21 10:38:53 -0700

    Got State into Props.
    Setting up the environment. mapStateToProps - gets the information in.

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
+import BookList from '../containers/book-list';
 
-export default class App extends Component {
+export default class App extends React.Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <BookList />
+      </div>
     );
   }
 }
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'JavaScript: The Good Parts' },
+    { title: 'Emotional Agility' },
+    { title: 'Harry Potter' },
+    { title: 'Eloquent Ruby' }
+  ]
+}
